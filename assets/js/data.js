d14901ee3518bf1121be4cfb2fe3d8f1c841e31a
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-10-14 18:07:46 -0400

    adding the starter files Paul made with a couple of changes to the HTML I added to help me differentiate between them

---
+++ assets/js/data.js
@@ -0,0 +1,8 @@
+// list of all questions, choices, and answers
+var data = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    correct: "alerts",
+  },
+];
---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+// STEP 1: Get HTML elements setup so we can manipulate them with JS for our quiz
+
+// STEP 2: Connect multiple .js files to use separate file for data (questions)
+
+// STEP 3: Hide instructions when game starts then display a question with possible answers
+
+// STEP 4: Clicking on an answer will display "Correct!" or "Wrong." in our feedback element
+// then bring us to the next question
+
+// STEP 5: When all questions are done, prompt user for intiials
